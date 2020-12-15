192b3612d65711f5b50e14058108db686a4b3b89
Vincent A <71930091+VincentA55@users.noreply.github.com>
2020-12-15 22:17:44 +0000

    adding other classes

---
+++ BlueJ-App06/Item.java
@@ -0,0 +1,30 @@
+
+/**
+ * A class for the various items a player must collect to complete
+ * the game
+ *
+ * @author Vincent A.
+ * @version 15/12/20
+ */
+public class Item
+{
+    private String name;
+    
+    private Room location;
+    
+    private Room useLocation;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item(String name, Room location, Room useLocation)
+    {
+        this.name = name;
+        this.location = location; 
+        this.useLocation = useLocation;
+    }
+    
+    
+
+    
+}
---
+++ BlueJ-App06/Player.java
@@ -0,0 +1,24 @@
+
+/**
+ * A player class that represents the user
+ * it contains the players Energy, inventory, and location
+ *
+ * @author Vincent A.
+ * @version 15/12/20
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    
+}
